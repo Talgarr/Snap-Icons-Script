a40da3e1041dc633cc16ea4862bc3381ddb8a672
Sébastien Graveline <graveline.seb+dev@gmail.com>
2024-10-02 13:34:46 -0400

    use map for not standard icon

--- replace-script.sh
+++ replace-script.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-ICONS_PATH=/usr/share/icons/candy-icons/apps/scalable 
+ICONS_PATH=/usr/share/icons/candy-icons/apps/scalable
 SNAP_PATH=/var/lib/snapd/desktop/applications/
+
+declare -A alt_icons=(
+    ["snap-store"]="software-store.svg"
+    ["dbeaver-ce"]="dbeaver.svg"
+)
 
 files=$(find $SNAP_PATH -name "*.desktop" 2>/dev/null)
 for file in $files
@@ -12,17 +17,9 @@ do
     icon="default.svg"
     if [ -z "$poss" ]; then
         echo "No matching icon found for $base_name"
-        case $base_name in
-            "snap-store")
-                icon="software-store.svg"
-                ;;
-            "dbeaver-ce")
-                icon="dbeaver.svg"
-                ;;
-            *)
-                echo "No alternative icon."
-                ;;
-        esac
+        if [ -n "${alt_icons[$base_name]}" ]; then
+            icon=${alt_icons[$base_name]}
+        fi
     elif [ -f "$ICONS_PATH/$base_name.svg" ]; then
         icon="$base_name.svg"
     else
